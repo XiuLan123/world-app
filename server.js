cc6ff4cc69a4946c3000db17aaa412557e82b177
John Zhang <1724118498@qq.com>
2021-04-21 08:11:35 +0800

    push

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3001;
+const unirest = require("unirest");
+const API_KEY = "96cb3694-3286-4ec0-9ff7-697250fc833f";
+
+app.get('/api/associations/:word', (req, res) => {
+	const word = req.params.word;
+	const request = unirest.get(`https://api.wordassociations.net/associations/v1.0/json/search?apikey=${API_KEY}&lang=en&text=${word}`)
+	.then(response => {
+		const results = response.body.response[0].items || []; // grab array of results
+		console.log(`Num results=${results.length}`);
+		res.json(results);
+	})
+	.catch(error => {
+		console.log(`error=${error}`);
+		res.json({status:"Error", message: `${error}`});
+	});
+});
+
+app.listen(port, () => {
+  console.log(`word-app listening on port ${port}`);
+});
