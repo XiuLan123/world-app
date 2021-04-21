e85f84d007b63316033695f45314ee5132a8dbf9
John Zhang <1724118498@qq.com>
2021-04-21 08:32:40 +0800

    push

--- server.js
+++ server.js
@@ -7,17 +7,34 @@ const API_KEY = "96cb3694-3286-4ec0-9ff7-697250fc833f";
 app.get('/api/associations/:word', (req, res) => {
 	const word = req.params.word;
 	const request = unirest.get(`https://api.wordassociations.net/associations/v1.0/json/search?apikey=${API_KEY}&lang=en&text=${word}`)
-	.then(response => {
-		const results = response.body.response[0].items || []; // grab array of results
-		console.log(`Num results=${results.length}`);
-		res.json(results);
-	})
-	.catch(error => {
-		console.log(`error=${error}`);
-		res.json({status:"Error", message: `${error}`});
-	});
+		.then(response => {
+			const results = response.body.response[0].items || []; // grab array of results
+			console.log(`Num results=${results.length}`);
+			res.json(results);
+		})
+		.catch(error => {
+			console.log(`error=${error}`);
+			res.json({
+				status: "Error",
+				message: `${error}`
+			});
+		});
 });
 
 app.listen(port, () => {
-  console.log(`word-app listening on port ${port}`);
+	console.log(`word-app listening on port ${port}`);
+});
+
+// import path library
+const path = require('path');
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+// ...
+
+// put this AFTER the other `app.get(...`
+// for any request that doesn't match one above, send back React's index.html file
+app.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname + '/client/build/index.html'));
 });
